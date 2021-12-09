164bd874881f4df08acc84acc05003da8f964ee6
Lee Segal <segalee17@gmail.com>
2021-12-09 18:43:35 +0200

    starting point

---
+++ js/services/userService.js
@@ -0,0 +1,21 @@
+//  <!-- Here we will use a <form> to get the user settings and save them to
+//  localStorage.
+//  The application should use the colors provided by the user and show the
+//  pages accordingly.
+//  Step 1 - Colors
+//  Use HTML5 color <input> to let the user set its background and text color of the
+//  pages.
+//  TIP: use: userService.save(userData)
+//  Step 2 – Date and Time
+//  Use HTML5 date and time <input>s to let the user set his exact birth time, In the
+//  hompage render how many hours to his next (accurate) birthday
+//  Step 3 – Wrap in a form
+//  Put those inputs in a <form>, and when submit, use a service to keep them in a
+//  localstorage object: userData
+//  TIP: you will need event.preventDefault in the onsubmit event handler.
+//  Step 4 – Add some more inputs
+//  • Add a required email <input>
+//  • Add a range <input> to let the user select his age: 18->120
+//   -->
+
+// <!-- <section, nav, main, aside, header,footer> -->
